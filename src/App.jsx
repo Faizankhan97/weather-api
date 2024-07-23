488350bf40d8ad7549a0284e5ab3c1a87cdd7542
Faizan Asad <faizan.asad@merisehat.app>
2024-07-23 10:16:58 +0500

    weather api

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <div className="weather">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
