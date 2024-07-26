60f29809968a4b8b5a2b3c7777d8d0a1132ce92c
Faizan Asad <faizan.asad@merisehat.app>
2024-07-26 10:36:55 +0500

    weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import SearchIcon from './assets/search.png';
+import Cloud from './assets/cloud.png';
 
 function App() {
 
@@ -7,7 +9,15 @@ function App() {
     <>
       <div className='container'>
         <div className="weather">
-          
+          <div className="search">
+            <input type="text" placeholder='Enter City Name' />
+            <button><img src={SearchIcon} alt="" /></button>
+          </div>
+          <div className='winfo'>
+            <img src={Cloud} alt="" className='icon' />
+            <h1>22°C</h1>
+            <h2>London</h2>
+          </div>
         </div>
       </div>
     </>
